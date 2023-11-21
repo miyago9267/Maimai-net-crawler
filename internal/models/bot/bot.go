d2898ef1dd60e637271a9500076d032c15040529
miyago9267 <miyagomizugen@gmail.com>
2023-11-22 00:59:16 +0800

    feat: complete command and sync value

--- internal/models/bot/bot.go
+++ internal/models/bot/bot.go
@@ -1,6 +1,7 @@
 package bot
 
 import (
+	"bytes"
 	"discordbot/internal/models/crawler"
 	"fmt"
 	"log"
@@ -14,11 +15,16 @@ import (
 var (
 	OpenWeatherToken string
 	BotToken         string
+	SID              string
+	Password         string
 )
 
-func Run() {
+func Run(sid string, pwd string) {
+
 	// Create new Discord Session
 	discord, err := discordgo.New("Bot " + BotToken)
+	SID = sid
+	Password = pwd
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -40,22 +46,25 @@ func Run() {
 }
 
 func newMessage(discord *discordgo.Session, message *discordgo.MessageCreate) {
-	log.Println(message.Content)
+	// log.Println(message.Content)
 
 	// Ignore bot messaage
 	if message.Author.ID == discord.State.User.ID {
 		return
 	}
 
-	// Respond to messages
-	switch {
-	case strings.Contains(message.Content, "weather"):
-		discord.ChannelMessageSend(message.ChannelID, "I can help with that!")
-	case strings.Contains(message.Content, "bot"):
-		discord.ChannelMessageSend(message.ChannelID, "Hi there!")
-	case strings.Contains(message.Content, "maimai"):
-		discord.ChannelMessageSend(message.ChannelID, "幹 你戳到測試了 操 我還在搞Golang的機器人")
-		crawler.Run("8058430092794")
-		// discord.ChannelMessageSend(message.ChannelID, crawler.Run("8058430092794"))
+	if strings.HasPrefix(message.Content, "&maimai") {
+		args := strings.Fields(message.Content)
+
+		if len(args) < 2 {
+			discord.ChannelMessageSend(message.ChannelID, "請提供好友ID.")
+			return
+		}
+
+		id := args[1]
+		// log.Println(id)
+		pic := crawler.Run(id, SID, Password)
+		reader := bytes.NewReader(pic)
+		discord.ChannelFileSendWithMessage(message.ChannelID, "", "maimai.png", reader)
 	}
 }
